1ed2a309e17bec4e45ad50eda38d499785b48510
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-05-07 21:32:52 -0400

    Started correct integer function in a_null_encounter.py.

--- a_null_encounter.py
+++ a_null_encounter.py
@@ -5,23 +5,23 @@
 # where the user is trapped in a void and
 # meets someone named null, an entity who
 # later tells the user their story. The user
-# must endure the harmful environment of 
-# the void and answer stages of questions 
+# must endure the harmful environment of
+# the void and answer stages of questions
 # to win.
 
-# Define the function to help get the correct string
-# from the user input.
-def get_correct_string(prompt, target_str, lives):
 
-    # Replicate a do..while loop by constructing an infinite loop.
+# Define a function to help get the correct
+# string answer from the user input.
+def get_correct_string(prompt, target_str, lives):
+    # Replicate a do..while by constructing an infinite loop.
     while True:
         # Display the prompt to the user and get their
-        # answer, regardless of case.
+        # string answer, regardless of case.
         user_str_input = input(prompt).lower()
 
-        # Check if the user entered an input that did not 
+        # Check if the user entered an input that did not
         # match the answer, meaning it was incorrect.
-        if (user_str_input != target_str):
+        if user_str_input != target_str:
             # Take away one life from the user
             # and keep it from going below zero.
             lives = max(lives - 1, 0)
@@ -32,7 +32,9 @@ def get_correct_string(prompt, target_str, lives):
         else:
             # Display to the user that they were correct,
             # along with their resulting lives.
-            print(f"Correct. You currently have {lives} {'life' if lives == 1 else 'lives'}.")
+            print(
+                f"Correct. You currently have {lives} {'life' if lives == 1 else 'lives'}."
+            )
 
         # Check if the user input was the correct answer or
         # if their lives are less than or equal to zero.
@@ -40,11 +42,23 @@ def get_correct_string(prompt, target_str, lives):
             # Break the loop.
             break
 
+
+# Define a function to help get the correct
+# integer answer from the user input.
+def get_correct_integer(prompt, target_int, lives):
+    # Replicate a do..while by constructing an infinite loop.
+    while True:
+        # Display the prompt to the user and get
+        # their integer answer as a string.
+        user_int_input_str = input(prompt)
+
+       
         
+
 
 # Define the main function.
 def main():
-    get_correct_string("what: ", "yes", 5)
+    pass
 
 
 # Check if the special name of the file is __main__.
